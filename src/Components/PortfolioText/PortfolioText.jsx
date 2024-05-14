9d1841cb304076d4f79bba4eb161c38e421d0511
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2024-05-15 00:13:08 +0600

    marquee

--- src/Components/PortfolioText/PortfolioText.jsx
+++ src/Components/PortfolioText/PortfolioText.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Marquee from "react-fast-marquee";
+import { FaRegStar } from "react-icons/fa6";
 
 const PortfolioText = () => {
     return (
-        <div>PortfolioText</div>
+        <section className='bg-zinc-900 text-white'>
+            <div className="w-full py-10 bg-[rgb(0,77,67)] ">
+                <div className="">
+                    <Marquee speed={150}>
+                        <div className='px-10 text-4xl '><FaRegStar /></div>
+
+                        <div className="text-6xl  uppercase font-semibold tracking-tighter ">Ui/ UX Design</div>
+
+
+                        <div className='px-10 text-4xl '><FaRegStar /></div>
+
+                        <div className="text-6xl  uppercase font-semibold tracking-tighter">Website development</div>
+
+                        <div className='px-10 text-4xl '><FaRegStar /></div>
+                        <div className="text-6xl  uppercase font-semibold tracking-tighter ">Search Engine Optimaization (SEO)</div>
+                        <div className='px-10 text-4xl '><FaRegStar /></div>
+                        <div className="text-6xl  uppercase font-semibold tracking-tighter ">Social Media Marketing</div>
+                    </Marquee >
+
+                </div >
+
+            </div >
+
+        </section >
     )
 }
 
